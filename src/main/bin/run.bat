aef8804573ea44008be97487fd14ca5ff3ca3d51
lizengqiang <lzq@justep.com>
2016-10-20 06:06:07 +0000

    修改启动服务时的参数配置
    git-svn-id: http://192.168.1.20/svn/im/trunk/dangchat-bots@685 0c17fa83-6788-ff4f-83ba-5dfcf0814e92

--- src/main/bin/run.bat
+++ src/main/bin/run.bat
@@ -7,6 +7,10 @@
 @echo off
 
 if "%DANGCHAT_BOTS_HOME%"=="" set "DANGCHAT_BOTS_HOME=%~dp0\\.."
-echo "%DANGCHAT_BOTS_HOME%"
+rem @echo "%DANGCHAT_BOTS_HOME%"
 
-java -jar -Xms256M -Xmx256M -server %DANGCHAT_BOTS_HOME%/lib/dangchat-bots-0.8.jar
+if "%DANGCHAT_BOTS_JAVA_HOME%"=="" set "DANGCHAT_BOTS_JAVA_HOME=%~dp0\\..\\.."
+
+rem @echo %DANGCHAT_BOTS_JAVA_HOME%
+
+%DANGCHAT_BOTS_JAVA_HOME%\java\jre1.8\bin\java -jar -Xms256M -Xmx256M -server %DANGCHAT_BOTS_HOME%/lib/dangchat-bots-0.8.jar
